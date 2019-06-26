ff3d15289b3f3db17c6f0bfcc14a292ae96d7906
alwaysmaria1 <alwaysmaria1@gmail.com>
2019-06-26 06:25:31 -0700

    iteration 2

--- viewFinder/AddPhotoViewController.swift
+++ viewFinder/AddPhotoViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
+
 class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
     var image = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         image.delegate = self
@@ -21,18 +24,42 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         image.sourceType = .camera
         present(image, animated: true, completion: nil)
     }
-    // Do any additional setup after loading the view.
-        
+    
+    // open the library and choose a picture
     @IBAction func openLibrary(_ sender: UIButton) {
         image.sourceType = .photoLibrary
         present(image, animated: true, completion: nil)
     }
-    
+    // name for the image view square in the viewcontroller
     @IBOutlet weak var displayImage: UIImageView!
     
+    // displays image on the image view square??
     internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             displayImage.image = selectedImage
+        }
+    }
+    
+// name of text box that saves label with photo
+    @IBOutlet weak var captionText: UITextField!
+   
+    //save image
+    @IBAction func savePhoto(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            
+            photoToSave.caption = captionText.text
+            
+            if let userImage = displayImage.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            
+            navigationController?.popViewController(animated: true)
         }
     }
     /*
